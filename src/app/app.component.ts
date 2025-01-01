ef7eed51f1df1f96401a051daea4ca6bad7e3e47
Matvey Bashlykov <bashmatred@gmail.com>
2025-01-02 02:08:59 +0300

    Add basic structure for boards page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LogInComponent } from "../log-in/log-in.component";
 import { SignUpComponent } from '../sign-up/sign-up.component';
+import { BoardsPageComponent } from "../boards-page/boards-page.component";
 
 @Component({
   selector: 'app',
-  imports: [RouterOutlet, LogInComponent, SignUpComponent],
+  imports: [RouterOutlet, LogInComponent, SignUpComponent, BoardsPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/boards-page/boards-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'boards-page',
+  imports: [RouterOutlet],
+  templateUrl: './boards-page.component.html',
+  styleUrl: './boards-page.component.css'
+})
+export class BoardsPageComponent {}
