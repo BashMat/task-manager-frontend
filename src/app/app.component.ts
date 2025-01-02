766fc9758b99d3cb053df36978b221ea0cabafa1
Matvey Bashlykov <bashmatred@gmail.com>
2025-01-02 15:28:01 +0300

    Add basic logic to read Access Token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { LogInComponent } from "../log-in/log-in.component";
 import { SignUpComponent } from '../sign-up/sign-up.component';
 import { BoardsPageComponent } from "../boards-page/boards-page.component";
+import { jwtDecode } from 'jwt-decode';
 
 @Component({
   selector: 'app',
@@ -10,4 +11,48 @@ import { BoardsPageComponent } from "../boards-page/boards-page.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent
+{
+  accessToken: string | null = this.GetAccessToken();
+  hasValidAccessToken: boolean = this.IsValidToken(this.accessToken);
+
+  GetAccessToken(): string | null
+  {
+    let localStorageEntry = localStorage.getItem('token');
+    if (!localStorageEntry || localStorageEntry === null || localStorageEntry === "null")
+    {
+      return null;
+    }
+    return localStorageEntry;
+  }
+
+  IsValidToken(token: string | null): boolean
+  {
+    if (token === null)
+    {
+      console.log("Token is null");
+      return false;
+    }
+
+    let decodedToken = jwtDecode(token);
+    let currentDate = new Date();
+
+    if (decodedToken.exp === null)
+    {
+      console.log("Token invalid.");
+      return false;
+    }
+
+    if (decodedToken.exp! * 1000 < currentDate.getTime())
+    {
+        console.log("Token expired.");
+        return false;
+    } 
+    else 
+    {
+        console.log("Valid token");
+        // TODO: add request to check token at backend
+        return true;
+    }
+  }
+}
